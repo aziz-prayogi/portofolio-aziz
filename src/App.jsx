6a337c7fa4bd4c02fae385fa1845da135e44ae6d
Aziz Prayogi <azizprayogi2008@gmail.com>
2026-06-15 00:15:01 +0700

    fix: memperbaiki certificate tidak terdeteksi

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavbarComponent from "./components/NavbarComponent";
 import Home from "./pages/Home";
 import ProjectDetail from "./pages/ProjectDetail";
 import Projects from "./pages/Projects";
-import Certificates from "./pages/Certificates";
+import Certificates from "./pages/Certificates"; ///Users/yogi/Documents/imam ahmad aziz prayogi/project/portofolio-v3/src/pages/Certificates.jsx
 import FooterComponent from "./components/FooterComponent"; 
 
 function App() {
